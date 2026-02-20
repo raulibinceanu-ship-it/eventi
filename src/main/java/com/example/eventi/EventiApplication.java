123ab2bf153074e9b928bf2df2661ee07e35baad
raulibinceanu-ship-it <raulibinceanu@gmail.com>
2026-02-20 13:50:09 +0100

    Initial commit

---
+++ src/main/java/com/example/eventi/EventiApplication.java
@@ -0,0 +1,13 @@
+package com.example.eventi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/eventi/EventiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eventi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
